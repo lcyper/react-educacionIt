a4eabfe620222d451666486a7ac9cb0322699b2b
lcyper <lcyper@gmail.com>
2020-08-02 18:12:24 -0300

    arreglando git

--- src/ui/componentes/Contador.jsx
+++ src/ui/componentes/Contador.jsx
@@ -2,20 +2,13 @@ import React from "react"
 
 let Contador = ({ contador, manejarContador,nombre = "contador"}) => {
     return (
-<<<<<<< HEAD
         <div className="contador">
-=======
-        <>
->>>>>>> d1d3f1e6eff0a3069bc88b68f603990657574465
             <p>{nombre} : {contador}</p>
             <button onClick={manejarContador.bind(null, nombre, -1)}>-</button>
             <button onClick={manejarContador.bind(null, nombre, 0)}>resetear</button>
             <button onClick={manejarContador.bind(null, nombre, 1)} >+</button>
-<<<<<<< HEAD
         </div>
-=======
-        </>
->>>>>>> d1d3f1e6eff0a3069bc88b68f603990657574465
+
     )
 }
 
